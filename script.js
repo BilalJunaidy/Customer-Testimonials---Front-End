17770336bffb066539eeb38219bb8adb11b2ab5d
Bilal Junaidy <bilal.junaidy@gmail.com>
2020-12-29 14:20:31 +0500

    Expanding cards for customer testimonials now working

---
+++ script.js
@@ -0,0 +1,24 @@
+// function expand() {
+//     document.querySelectorAll('img').forEach(function(elem) {
+//         elem.style.width = 300px;
+//     }
+// }
+
+
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        RemoveActiveClass()
+        panel.classList.add('active');
+    })
+})
+
+
+function RemoveActiveClass() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active')
+    })
+}
+
